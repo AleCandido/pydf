1239fea8a588d6be409a7d68a3288ff1e693c385
Alessandro Candido <candido.ale@gmail.com>
2022-06-18 18:08:27 +0200

    Import Juan Cruz-Martinez lhapdf_management as CLI base

---
+++ src/pydf_cli/configuration.py
@@ -0,0 +1,9 @@
+"""
+    LHAPDF configuration environment
+
+    It exposes an object (environment) that should contain
+    all relevant external information
+"""
+from pydf import configuration
+
+environment = configuration.Environment()
