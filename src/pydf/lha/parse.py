44bc517a1e0f5f0c188a52b980533fdc1454e49c
Alessandro Candido <candido.ale@gmail.com>
2022-06-19 08:00:14 +0200

    Parse info file and start members

--- src/pydf/lha/parse.py
+++ src/pydf/lha/parse.py
@@ -1,22 +1,39 @@
 # -*- coding: utf-8 -*-
 """Read LHA format."""
+import pathlib
+
 import numpy as np
 import yaml
 
 from ..pdf import PDF
-from . import configuration as conf
+from . import path
+
+
+def member(path: pathlib.Path) -> tuple[dict, list[np.ndarray]]:
+    """Parse PDF member file."""
+    content = path.read_text(encoding="utf-8")
+
+    parts = content.split("---")
+
+    header = yaml.safe_load(parts[0])
+    for patch in parts[1:]:
+        __import__("pdb").set_trace()
+        pass
+
+    return header, [np.array([])]
 
 
 def parse(setname: str) -> PDF:
     """Parse PDF in LHA format."""
-    pdfdir = conf.lhapdf_datapath() / setname
-
-    if not pdfdir.exists():
-        raise FileNotFoundError(
-            f"No PDF set '{setname}' available in '{pdfdir.parent}'"
-        )
+    pdfdir = path.locate(setname)
 
     infopath = pdfdir / f"{setname}.info"
     info = yaml.safe_load(infopath.read_text(encoding="utf-8"))
+
+    members = []
+    for memberpath in sorted(pdfdir.glob("*.dat")):
+        print(memberpath)
+        head, patches = member(memberpath)
+        members.append(patches)
 
     return PDF(np.array([]), np.array([]), np.array([]), np.array([]), info=info)
